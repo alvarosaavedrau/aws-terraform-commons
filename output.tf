7fcb77ed51718bdb47fa782fe5950cd487f94ce8
ASPU <alvarosaavedrau@gmail.com>
2024-04-17 22:38:57 +0200

    terraform format fmt

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "ec2_public_ip" {
-    value = aws_instance.public.public_ip
+  value = aws_instance.public.public_ip
 }
 
 output "ec2_private_ip" {
-    value = aws_instance.private.private_ip
+  value = aws_instance.private.private_ip
 }
