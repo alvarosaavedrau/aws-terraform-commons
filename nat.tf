7ee4babd3d2eb71b6dff3a2e0e52749d7b8941f7
ASPU <alvarosaavedrau@gmail.com>
2024-04-21 15:14:44 +0200

    simplify the infrastructure, no us-east-1b

--- nat.tf
+++ nat.tf
@@ -8,16 +8,6 @@ resource "aws_eip" "publicA" {
   )
 }
 
-resource "aws_eip" "publicB" {
-
-  tags = merge(
-    var.common_tags,
-    {
-      Name = "eip-${var.name}-publicB"
-    }
-  )
-}
-
 resource "aws_nat_gateway" "publicA" {
   subnet_id         = aws_subnet.publicA.id
   allocation_id     = aws_eip.publicA.id
@@ -27,19 +17,6 @@ resource "aws_nat_gateway" "publicA" {
     var.common_tags,
     {
       Name = "nat-gateway-${var.name}-publicA"
-    }
-  )
-}
-
-resource "aws_nat_gateway" "publicB" {
-  subnet_id         = aws_subnet.publicB.id
-  allocation_id     = aws_eip.publicB.id
-  connectivity_type = "public"
-
-  tags = merge(
-    var.common_tags,
-    {
-      Name = "nat-gateway-${var.name}-publicB"
     }
   )
 }
--- vpc.tf
+++ vpc.tf
@@ -27,22 +27,6 @@ resource "aws_subnet" "publicA" {
   depends_on = [aws_vpc.dev]
 }
 
-resource "aws_subnet" "publicB" {
-  vpc_id                  = aws_vpc.dev.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = merge(
-    var.common_tags,
-    {
-      Name = "subnet-${var.name}-publicB"
-    }
-  )
-
-  depends_on = [aws_subnet.publicA]
-}
-
 resource "aws_subnet" "privateA" {
   vpc_id                  = aws_vpc.dev.id
   cidr_block              = "10.0.16.0/20"
@@ -56,21 +40,4 @@ resource "aws_subnet" "privateA" {
     }
   )
 
-  depends_on = [aws_subnet.publicB]
-}
-
-resource "aws_subnet" "privateB" {
-  vpc_id                  = aws_vpc.dev.id
-  cidr_block              = "10.0.32.0/20"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = false
-
-  tags = merge(
-    var.common_tags,
-    {
-      Name = "subnet-${var.name}-privateB"
-    }
-  )
-
-  depends_on = [aws_subnet.privateA]
 }
