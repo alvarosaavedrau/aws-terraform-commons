317538db1f8466828c0bfd2f308b38b4c0b059bc
ASPU <alvarosaavedrau@gmail.com>
2024-05-18 21:59:33 +0200

    add provider and aws versions

--- provider.tf
+++ provider.tf
@@ -4,3 +4,13 @@ provider "aws" {
   secret_key            = var.secret_key
   forbidden_account_ids = var.personal_account_id
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.50.0"
+    }
+  }
+  required_version = ">= 1.8.2"
+}
