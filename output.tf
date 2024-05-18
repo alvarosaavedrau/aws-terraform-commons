f3935d6e6c48012b345f5ee2431a97fe2eb4ba54
alvarosaavedrau <cloud@alvarosaavedra.net>
2024-05-19 01:19:34 +0200

    add rds endpoints

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "ec2_public_ip" {
 output "ec2_private_ip" {
   value = aws_instance.private.private_ip
 }
+
+output "rds_reader_endpoint" {
+  value = aws_rds_cluster.postgresql.reader_endpoint
+}
+
+output "rds_writer_endpoint" {
+  value = aws_rds_cluster.postgresql.endpoint
+}
