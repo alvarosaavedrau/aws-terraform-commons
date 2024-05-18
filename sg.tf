56f15a39d078da2c6bf36272ec94e3409061aa0c
alvarosaavedrau <cloud@alvarosaavedra.net>
2024-05-18 22:09:19 +0200

    fix sg reference

--- sg.tf
+++ sg.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "private" {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    security_groups = [aws_security_group.main.id]
+    security_groups = [aws_security_group.public.id]
   }
 
   egress {
