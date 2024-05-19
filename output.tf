784cd4aacd773eaded2a5f0174f7a927b4b671d6
alvarosaavedrau <cloud@alvarosaavedra.net>
2024-05-19 22:17:08 +0200

    removed rds cluster outputs and added instances endpoints

--- output.tf
+++ output.tf
@@ -6,21 +6,29 @@ output "ec2_private_ip" {
   value = aws_instance.private.private_ip
 }
 
-output "rds_cluster_writer_endpoint" {
-  value = aws_rds_cluster.rds_cluster.endpoint
-}
-output "rds_cluster_reader_endpoint" {
-  value = aws_rds_cluster.rds_cluster.reader_endpoint
-}
+# output "rds_cluster_writer_endpoint" {
+#   value = aws_rds_cluster.rds_cluster.endpoint
+# }
+# output "rds_cluster_reader_endpoint" {
+#   value = aws_rds_cluster.rds_cluster.reader_endpoint
+# }
 
 output "rds_cluster_database_name" {
   value = aws_rds_cluster.rds_cluster.database_name
 }
 
-output "aws_rds_cluster_instance" {
+output "aws_rds_cluster_instance_writer" {
   value = aws_rds_cluster_instance.rds_instance.endpoint
+}
+
+output "aws_rds_cluster_instance_reader" {
+  value = aws_rds_cluster_instance.rds_replica_reader.endpoint
 }
 
 output "aws_rds_cluster_username" {
   value = aws_rds_cluster.rds_cluster.master_username
+}
+
+output "aws_rds_port" {
+  value = aws_rds_cluster.rds_cluster.port
 }
