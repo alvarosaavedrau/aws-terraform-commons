f0af5bd460347dbfc6f122e928d19bbdd42eef2a
alvarosaavedrau <cloud@alvarosaavedra.net>
2024-05-19 16:06:24 +0200

    add more outputs

--- output.tf
+++ output.tf
@@ -6,10 +6,17 @@ output "ec2_private_ip" {
   value = aws_instance.private.private_ip
 }
 
-output "rds_reader_endpoint" {
-  value = aws_rds_cluster.postgresql.reader_endpoint
+output "rds_cluster_writer_endpoint" {
+  value = aws_rds_cluster.rds_cluster.endpoint
+}
+output "rds_cluster_reader_endpoint" {
+  value = aws_rds_cluster.rds_cluster.reader_endpoint
 }
 
-output "rds_writer_endpoint" {
-  value = aws_rds_cluster.postgresql.endpoint
+output "rds_cluster_database_name" {
+  value = aws_rds_cluster.rds_cluster.database_name
+}
+
+output "aws_rds_cluster_instance" {
+  value = aws_rds_cluster_instance.rds_instance.endpoint
 }
