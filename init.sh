11fe9adfdadcbfc9798f9a196663040bd6f362a3
alvarosaavedrau <cloud@alvarosaavedra.net>
2024-05-19 15:56:47 +0200

    add mysql client installation to bastion

--- init.sh
+++ init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+echo "Installing postgresql client..."
+
 dnf install postgresql15.x86_64 -y
+
+echo "Installing mysql client..."
+
+sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
+sudo dnf install mysql80-community-release-el9-1.noarch.rpm -y
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
+sudo dnf install mysql-community-client -y
